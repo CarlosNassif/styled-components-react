fe0d6e215e903b802eb412035a07bea169781e54
Carlos Nassif <costa.carlos12@gmail.com>
2023-02-28 20:35:59 -0300

    Aula 5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import './App.css';
 
-import Button from './components/Button';
-import ReverseButton from './components/CustomButton';
-import TomatoButton from './components/TomatoButton';
+import { Link, StyledLink } from './components/Link';
 import Wrapper from './components/Wrapper';
 
 function App() {
   return (
     <div className='App'>
       <Wrapper>
-        <Button>Normal Button Without link</Button>
-        <Button as='a' href='#'>
-          Normal Button with link
-        </Button>
-        <TomatoButton as='a' href='#'>
-          TomatoButton with link
-        </TomatoButton>
-        <Button as={ReverseButton}>
-          Custom Button with Normal Button styles
-        </Button>
+        <Link>Unstyled, boring link</Link>
+        <br />
+        <StyledLink>Styled, exciting link</StyledLink>
       </Wrapper>
     </div>
   );
