0af6e2c913f6cc46b69e841cc6bc3cb86189a607
Carlos Nassif <costa.carlos12@gmail.com>
2023-02-28 20:32:45 -0300

    Aula 4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
-import Button from './components/ButtonWithProps';
 
-import Title from './components/Title';
+import Button from './components/Button';
+import ReverseButton from './components/CustomButton';
+import TomatoButton from './components/TomatoButton';
 import Wrapper from './components/Wrapper';
 
 function App() {
   return (
     <div className='App'>
       <Wrapper>
-        <Button>Normal</Button>
-        <Button primary>Button with props</Button>
+        <Button>Normal Button Without link</Button>
+        <Button as='a' href='#'>
+          Normal Button with link
+        </Button>
+        <TomatoButton as='a' href='#'>
+          TomatoButton with link
+        </TomatoButton>
+        <Button as={ReverseButton}>
+          Custom Button with Normal Button styles
+        </Button>
       </Wrapper>
     </div>
   );
